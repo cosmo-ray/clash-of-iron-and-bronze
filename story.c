fbbbe3929eb353b7125ac1c5932e826ba6994c55
Uso Pine Arch <uso.cosmo.ray@gmail.com>
2020-12-01 03:02:04 +0100

    continue story !
    Signed-off-by: Uso Pine Arch <uso.cosmo.ray@gmail.com>

--- story.c
+++ story.c
@@ -18,7 +18,6 @@ void *dungeon_story_init(int nbArgs, void **args)
 	Entity *e = args[0];
 	printf("dungeon_story_init %s\n", yeGetStringAt(e, "file"));
 	yeAutoFree Entity *fe =  ygFileToEnt(YJSON, yeGetStringAt(e, "file"), NULL);
-	Entity *next = yeCreateArray(e, "next");
 	yeAutoFree Entity *lvls = yeCreateArray(NULL, NULL);
 
 	YEntityBlock {
@@ -29,23 +28,45 @@ void *dungeon_story_init(int nbArgs, void **args)
 		lvls.slinger = 0;
 		lvls.hoplite = 0;
 		lvls.sear = 0;
-		lvls.legioness = 5;
-		next["<type>"] = "dungeon-fight";
+		lvls.legioness = 0;
 	}
 
-	Entity *cur_s = yeGet(fe, 0);
-	yeGetPush2(cur_s, "pre-battle-txt", e, "text");
-	yeGetPush(cur_s, next, "exclude");
-	yeGetPush(cur_s, next, "enemies");
-	printf("callNext: %p\n", ygGet("callNext"));
-	yePushBack(next, ygGet("callNext"), "win-action");
-	yeCreateCopy(lvls, next, "lvls");
+	Entity *cur_s;
+	Entity *tmpe = e;
+	Entity *last;
 
-	Entity *last = yeCreateArray(next, "next");
-	yeCreateString("text-screen", last, "<type>");
-	yeCreateString("rgba: 255 255 255 255", last, "background");
-	yeGetPush2(cur_s, "win-txt", last, "text");
-	
+	for (int i = 0; (cur_s = yeGet(fe, i)) != NULL; ++i) {
+		if (i) {
+			yeReCreateString("nextOnKeyDown", last, "action");
+			tmpe = yeCreateArray(last, "next");			
+			yeCreateString("text-screen", tmpe, "<type>");
+			yeCreateString("rgba: 255 255 255 255", tmpe, "background");
+			yeCreateString("nextOnKeyDown", tmpe, "action");
+		}
+		yeGetPush2(cur_s, "pre-battle-txt", tmpe, "text");
+
+		Entity *next = yeCreateArray(tmpe, "next");
+		yeCreateString("dungeon-fight", next, "<type>");
+		yeGetPush(cur_s, next, "exclude");
+		yeGetPush(cur_s, next, "enemies");
+		printf("callNext: %p\n", ygGet("callNext"));
+		yePushBack(next, ygGet("callNext"), "win-action");
+		printf("%s\n", yeToCStr(lvls, -1, YE_FORMAT_PRETTY));
+		yeCreateCopy(lvls, next, "lvls");
+		YE_FOREACH(lvls, l) {
+			yeAdd(l, 1);
+		}
+		YE_FOREACH(yeGet(cur_s, "exclude"), ex) {
+			yeAddAt(lvls, yeGetString(ex), -1);
+		}
+
+		last = yeCreateArray(next, "next");
+		yeCreateString("text-screen", last, "<type>");
+		yeCreateString("rgba: 255 255 255 255", last, "background");
+		yeGetPush2(cur_s, "win-txt", last, "text");
+		yeCreateString("QuitOnKeyDown", last, "action");
+	}	
+
 	void *ret = ywidNewWidget(e, "text-screen");
 	/* printf("%s\n", yeToCStr(e, -1, YE_FORMAT_PRETTY)); */
 	return ret;
